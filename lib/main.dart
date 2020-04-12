608cd3ed16ff2bf3c7fb3d33d8930e72e761aa7c
Luis <luenmuvel@gmail.com>
2020-04-12 12:45:11 -0500

    Rutas con nombre

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'package:componentes/src/pages/alert_page.dart';
+import 'package:componentes/src/pages/avatar_page.dart';
+
 import 'package:componentes/src/pages/home_page.dart';
-// import 'package:componentes/src/pages/home_temp.dart';
  
 void main() => runApp(MyApp());
  
@@ -11,7 +13,13 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      home: HomePage(),
+      // home: HomePage(),
+      initialRoute: '/',
+      routes: <String, WidgetBuilder>{
+        '/':      (BuildContext context) => HomePage(),
+        'alert':  (BuildContext context) => AlertPage(),
+        'avatar': (BuildContext context) => AvatarPage(),
+      },
     );
   }
 }
--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -41,10 +41,12 @@ class HomePage extends StatelessWidget {
         leading: getIcon(opt['icon']),
         trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
         onTap: () {
-          final route = MaterialPageRoute(
-            builder: (context) => AlertPage()
-          );
-          Navigator.push(context, route);
+
+          Navigator.pushNamed(context, opt['ruta']);
+          // final route = MaterialPageRoute(
+          //   builder: (context) => AlertPage()
+          // );
+          // Navigator.push(context, route);
         },
       );
 
