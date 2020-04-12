8990dcc73888279628d24cbad45bf6ad1d49000d
Luis <luenmuvel@gmail.com>
2020-04-12 17:34:37 -0500

    Widget - Circle Avatar

--- lib/src/pages/avatar_page.dart
+++ lib/src/pages/avatar_page.dart
@@ -6,7 +6,29 @@ class AvatarPage extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text('Avatar Page'),
+        actions: <Widget>[
+          Container(
+            padding: EdgeInsets.all(7.0),
+            child: CircleAvatar(
+              backgroundImage: NetworkImage('https://as.com/meristation/imagenes/2018/11/12/noticia/1542053880_906934_1542962110_sumario_normal.jpeg'),
+              radius: 22.0,
+            ),
+          ),
+          Container(
+            margin: EdgeInsets.only(right: 10.0),
+            child: CircleAvatar(
+              child: Text('SL'),
+              backgroundColor: Colors.orange[200],
+            ),
+          )
+        ],
       ),
+      body: Center(
+        child: FadeInImage(
+          image: NetworkImage('https://cdn.tn.com.ar/sites/default/files/styles/720x720/public/2018/11/12/stanlee.jpg'),
+          placeholder: AssetImage('assets/original.gif')
+        ),
+      )
     );
   }
 }
