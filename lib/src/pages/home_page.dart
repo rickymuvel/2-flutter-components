8392f9407dea7d4012348525801bab625736731e
Luis <luenmuvel@gmail.com>
2020-04-12 12:14:07 -0500

    haciendo uso del FutureBuilder para hacer petición de información con tiempo indefinido, son como las promesas

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -15,27 +15,34 @@ class HomePage extends StatelessWidget {
   }
 
   Widget _lista() {
-    print(menuProvider.opciones);
-
-    return 
-      ListView(
-        children: _listaItems(),
-      );
+    
+    return FutureBuilder(
+      future: menuProvider.cargarData(),
+      // initialData: [],
+      builder: (context, AsyncSnapshot<List<dynamic>> snapshot) {
+        return 
+          ListView(
+            children: _listaItems(snapshot.data),
+          );  
+      },
+    );
     }
       
-  List<Widget> _listaItems() {
-    return [
-      ListTile(
-        title: Text('Hola mundo')
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo')
-      ),
-      Divider(),
-      ListTile(
-        title: Text('Hola mundo')
-      ),
-    ];
+  List<Widget> _listaItems(List<dynamic> data) {
+    final List<Widget> opciones = [];
+
+    data.forEach((opt) {
+
+      final widgetTemp = ListTile(
+        title: Text(opt['texto']),
+        leading: Icon(Icons.account_circle, color: Colors.blue),
+        trailing: Icon(Icons.keyboard_arrow_right, color: Colors.blue),
+        onTap: () {},
+      );
+
+      opciones..add(widgetTemp)
+              ..add(Divider());
+    });
+    return opciones;
   }
 }
--- lib/src/providers/menu_provider.dart
+++ lib/src/providers/menu_provider.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/services.dart' show rootBundle;
+import 'dart:convert';
 
 class _MenuProvider {
   List<dynamic> opciones = [];
 
   _MenuProvider() {
-    cargarData();
+    // cargarData();
   }
 
-  void cargarData() {
-    rootBundle.loadString('data/menu_opts.json')
-              .then((data) {
-                print(data);
-              });
+  Future<List<dynamic>> cargarData() async {
+    final resp = await rootBundle.loadString('data/menu_opts.json');
+    Map dataMap = json.decode(resp);
+    opciones = dataMap['rutas'];
+    return opciones;
   }
 }
 
