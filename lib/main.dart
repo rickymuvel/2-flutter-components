94691155848b5d5228b7315eb0c0ec0ce61b12e5
Luis <luenmuvel@gmail.com>
2020-04-12 15:58:31 -0500

    rutas generadas dinámicamente y archivo de rutas independiente

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:componentes/src/pages/alert_page.dart';
-import 'package:componentes/src/pages/avatar_page.dart';
-
-import 'package:componentes/src/pages/home_page.dart';
+import 'package:componentes/src/routes/routes.dart';
  
 void main() => runApp(MyApp());
  
@@ -13,12 +11,14 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Componentes App',
       debugShowCheckedModeBanner: false,
-      // home: HomePage(),
       initialRoute: '/',
-      routes: <String, WidgetBuilder>{
-        '/':      (BuildContext context) => HomePage(),
-        'alert':  (BuildContext context) => AlertPage(),
-        'avatar': (BuildContext context) => AvatarPage(),
+      routes: getApplicationRoutes(),
+      // cuando la ruta que se llama no está definida se llama a este método:
+      onGenerateRoute: ( RouteSettings settings ) {
+        print('Ruta llamada: ${settings.name}');
+        return MaterialPageRoute(
+          builder: (context) => AlertPage()
+        );
       },
     );
   }
