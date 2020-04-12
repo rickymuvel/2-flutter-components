ebb5be8b2fd7a214722667c1d1931b08fd1be677
Luis <luenmuvel@gmail.com>
2020-04-11 19:32:18 -0500

    creando el proyecto y definiendo Widgets sin estado

---
+++ lib/src/pages/home_temp.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+
+class HomePageTemp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Temp')
+    );
+  }
+}
+
+// stl -> Snipped para StateLessWidget
+// stf -> Snipped para StateFulWidget
