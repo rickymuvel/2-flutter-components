a63400f6caf2d0251dfdefa7b3a6855b1077d1db
Luis <luenmuvel@gmail.com>
2020-04-13 21:51:51 -0500

    Input Field

---
+++ lib/src/pages/input_page.dart
@@ -0,0 +1,58 @@
+import 'package:flutter/material.dart';
+
+class InputPage extends StatefulWidget {
+  @override
+  _InputPageState createState() => _InputPageState();
+}
+
+class _InputPageState extends State<InputPage> {
+  
+  String _nombre = '';
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Input de texto'),
+      ),
+      body: ListView(
+        padding: EdgeInsets.symmetric(horizontal: 10.0, vertical: 20.0),
+        children: <Widget>[
+          _crearInput(),
+          Divider(),
+          _crearPersona(),
+        ],
+      ),
+    );
+  }
+
+  Widget _crearInput() {
+    return TextField(
+      // autofocus: true,
+      textCapitalization: TextCapitalization.sentences,
+      decoration: InputDecoration(
+        counter: Text('Letras ${_nombre.length}'),
+        hintText: 'Nombre de la persona',
+        labelText: 'Nombre',
+        helperText: 'Solo es el nombre',
+        suffixIcon: Icon(Icons.accessibility),
+        icon: Icon(Icons.account_circle),
+        // prefixIcon: Icon(Icons.person_add),
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20.0)
+        )
+      ),
+      onChanged: (valor) {
+        setState(() {
+          _nombre = valor;
+        });
+      },
+    );
+  }
+
+  Widget _crearPersona() {
+    return ListTile(
+      title: Text('Nombre es $_nombre')
+    );
+  }
+}
