8af66db4a145779dbf43962e1c037992b3392905
Luis <luenmuvel@gmail.com>
2020-04-18 21:11:41 -0500

    dropdown y dropdownmenuitem

--- lib/src/pages/input_page.dart
+++ lib/src/pages/input_page.dart
@@ -12,6 +12,10 @@ class _InputPageState extends State<InputPage> {
   String _password = '';
   String _fecha = '';
 
+  String _opcionSeleccionada = 'Volar';
+
+  List<String> _poderes = ['Volar', 'Rayos X', 'Super Aliento', 'Super Fuerza'];
+
   TextEditingController _inputFieldDateCongroller = new TextEditingController();
 
   @override
@@ -30,6 +34,8 @@ class _InputPageState extends State<InputPage> {
           _crearPassword(),
           Divider(),
           _crearFecha(context),
+          Divider(),
+          _crearDropdown(context),
           Divider(),
           _crearPersona(),
         ],
@@ -64,6 +70,7 @@ class _InputPageState extends State<InputPage> {
     return ListTile(
       title: Text('Nombre es $_nombre'),
       subtitle: Text('Fecha es $_fecha'),
+      trailing: Text(_opcionSeleccionada),
     );
   }
 
@@ -142,5 +149,39 @@ class _InputPageState extends State<InputPage> {
         _inputFieldDateCongroller.text = _fecha;
       });
     }
+  }
+
+  List<DropdownMenuItem<String>> getOpcionesDropdown() {
+    List<DropdownMenuItem<String>> lista = new List();
+
+    _poderes.forEach((poder) {
+      lista.add(DropdownMenuItem(
+        child: Text(poder),
+        value: poder
+      ));
+    });
+    return lista;
+  }
+
+  Widget _crearDropdown(BuildContext context) {
+    return Row(children: <Widget>[
+      Icon(Icons.select_all),
+      SizedBox(width: 25.0),
+      Expanded(
+          child: DropdownButton(
+          value: _opcionSeleccionada,
+          items: getOpcionesDropdown(),
+          onChanged: (opt) {
+            setState(() {
+              _opcionSeleccionada = opt;
+            });
+          },
+        ),
+      )
+    ]);
+    
+    
+    
+    
   }
 }
